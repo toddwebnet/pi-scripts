90f9cee7433a603efb89db1f292fd4059cf98739
jtodd <toddwebnet@gmail.com>
2019-02-16 12:39:51 -0600

    updates

--- send-top.sh
+++ send-top.sh
@@ -3,6 +3,4 @@ source variables.sh
 
 TOP_URL=$(read_var TOP_URL .env)
 
-
-# source ./variables.sh
-echo "time=`top -l 1`" | curl -d @- $TOP_URL
+echo "top=`top -n 1 -b | tr '|' '^' | tr '\n' '|' `" | curl -d @- $TOP_URL
